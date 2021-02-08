3650f90296fcadeb3b9ff1ab960dd91ee7dd05e3
Arnaud Hicks <ahicks@adflux.net>
2021-02-08 15:50:49 +0100

    Added extra fixture

--- test/test_Personne.py
+++ test/test_Personne.py
@@ -1,5 +1,5 @@
 import pytest
-from datetime import datetime
+from datetime import datetime, timedelta
 from src.Personne import Personne
 
 
@@ -7,6 +7,10 @@ class TestPersonne:
     @pytest.fixture
     def adulte(self):
         return Personne("Marc", 45, datetime.today())
+
+    @pytest.fixture
+    def notBirthday(self):
+        return Personne("Jean", 12, datetime.today() - timedelta(days=1))
 
     def test_createPersonne(self, adulte):
         assert str(adulte) == "Nom : Marc, Age: 45"
@@ -24,3 +28,8 @@ class TestPersonne:
         assert adulte.age == 45
         adulte.checkDate()
         assert adulte.age == 46
+
+    def test_notBirthday(self, notBirthday):
+        assert notBirthday.age == 12
+        notBirthday.checkDate()
+        assert notBirthday.age == 12
